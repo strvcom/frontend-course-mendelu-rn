b2c5023ad15b275da35ea1172a3f6fba6f62d644
Matěj Polák <matej.polaak@gmail.com>
2023-04-27 12:11:59 +0200

    feat: talk prep

---
+++ src/screens/Dashboard/presets/index.ts
@@ -0,0 +1,22 @@
+export const todos = [
+  {
+    id: 1,
+    title: 'Single Todo',
+  },
+  {
+    id: 2,
+    title: 'Multiple Todos',
+  },
+  {
+    id: 3,
+    title: 'Make the list scrollable',
+  },
+  {
+    id: 4,
+    title: 'Make todos checkable',
+  },
+  {
+    id: 5,
+    title: 'Delete a todo',
+  },
+]
